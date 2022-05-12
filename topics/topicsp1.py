a4a37f6db25a528cd0e302be7ac1e2c050006cc1
Emerson Chao <echao011@gmail.com>
2022-05-11 17:25:51 -0700

    steering calib 4

--- topics/topicsp1.py
+++ topics/topicsp1.py
@@ -52,15 +52,15 @@ class topicsp1(Node):
 
         if self.laser_left_min < 0.22: # too close to left wall
             self.cmd.linear.x = 0.6
-            self.cmd.angular.z = 0.35
+            self.cmd.angular.z = 0.3
 
         elif self.laser_left_min > 0.28: #far from wall left wall
             self.cmd.linear.x = 0.6
-            self.cmd.angular.z = 0.35
+            self.cmd.angular.z = 0.3
 
         else: # perfect
             self.cmd.linear.x = 0.6
-            self.cmd.angular.z = 0.35
+            self.cmd.angular.z = 0.3
 
         # Publishing the cmd_vel values to topipc
         self.publisher_.publish(self.cmd)
