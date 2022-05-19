951edf3ae9c1edee8aea99e0ea8794acac6c1b47
Emerson Chao <echao011@gmail.com>
2022-05-19 04:27:47 -0700

    replace files with fleshed out ros2 package details

--- launch/motion_plan_launch_file.launch.py
+++ launch/motion_plan_launch_file.launch.py
@@ -1,11 +1,10 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
-
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            package='topics',
-            executable='topicsp1',
+            package='exercise31_pkg',
+            executable='exercise31',
             output='screen'),
     ])
--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 from setuptools import setup
-import os
-from glob import glob
 
-package_name = 'topics'
+package_name = 'motion_plan_pkg'
 
 setup(
     name=package_name,
@@ -12,7 +10,6 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name), glob('launch/*.launch.py'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -23,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'topicsp1 = topics.topicsp1:main'
+            'exercise31 = exercise31_pkg.exercise31:main' #tweak
         ],
     },
 )
